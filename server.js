80bc2c3f20aa87f6b98138c28bce894f3f6abae0
Aiden <aidendsouzaa04@gmail.com>
2024-05-10 19:12:14 -0400

    setup cookies

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const session = require("express-session");
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
 
 const path = require('path');
 
+const edenStore = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: 'mySessions'
+});
+
 app.use(session({
-    secret: "ohoiahdoihohohihiiiihohoihioadoi"
+    secret: process.env.SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 30},
+    store: edenStore,
+    resave: false,
+    saveUninitialized: false
 }))
 
 let registerRouter = require("./routes/register-router");
-let groupRouter = require("./routes/group-router");
 let loginRouter = require("./routes/login-router");
+let groupRouter = require("./routes/group-router");
 
 mongoose.connect('mongodb://localhost:27017/Eden')
 .then(() => console.log('Connected to MongoDB'))
@@ -23,8 +36,17 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/register', registerRouter);
-app.use('/group', groupRouter);
 app.use('/login', loginRouter);
+
+app.use((req, res, next) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+});
+
+app.use('/group', groupRouter);
 
 app.get('/welcome', (req, res) => {
     res.sendFile(path.join(__dirname, './public', 'welcome.html'));
