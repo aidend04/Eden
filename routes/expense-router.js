97b91826764839523a7906693c75f017c0d294b6
Aiden <aidendsouzaa04@gmail.com>
2024-07-02 21:28:18 +0530

    syntax change

--- routes/expense-router.js
+++ routes/expense-router.js
@@ -568,9 +568,9 @@ router.post('/upload', upload.array('myFiles', 15), async (req, res) => {
 
     let storeData = [];
     const { fileTypeFromBuffer } = await import('file-type');
-    
+
     for (let file of files){
-        const fileTypeResult = await fileType.fromBuffer(file.data);
+        const fileTypeResult = await fileTypeFromBuffer(file.data);
         const mimeType = fileTypeResult ? fileTypeResult.mime : "image/jpg";
         const prompt = "Based on the image, return me the name of the store, total amount spent (dont include the $ sign), look for something in format of MM/DD/YYYY and change it to YYYY-MM-DD if not there say no date, and a category based on these: Dining Out, Entertainment, Subscriptions, Groceries, Rent, Utilities, Amazon, and Misc (no period at the end of Misc), please seperate all responses via a , and dont add extra words";
         const image = {
